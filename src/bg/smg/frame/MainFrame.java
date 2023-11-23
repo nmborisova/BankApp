325da0807e871165ee688b3b4df8664f6003b2d4
n.m.borisova <K8sedrf@1q>
2023-11-23 14:47:38 +0200

    Scaled banner icon

--- src/bg/smg/frame/MainFrame.java
+++ src/bg/smg/frame/MainFrame.java
@@ -2,6 +2,7 @@ package bg.smg.frame;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class MainFrame extends JFrame  {
     private JLabel bannerLbl;
@@ -13,8 +14,14 @@ public class MainFrame extends JFrame  {
         this.setSize(600,500);
         this.setTitle("Bank App");
 
-        bannerIcn = new ImageIcon("resources/bankBanner.png");
+        //bannerIcn = new ImageIcon("resources/bankBanner.png");
+        Image image = new ImageIcon("resources/bankBanner.png").getImage();
+        Image scaledInstance = image.getScaledInstance(300, 75, Image.SCALE_SMOOTH);
+        bannerIcn = new ImageIcon(scaledInstance);
+
         bannerLbl = new JLabel(bannerIcn);
+        bannerLbl.setVerticalAlignment(SwingConstants.TOP);
+
 
         this.add(bannerLbl);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
